d3836fbe29ef11c644f62636fe5796371d94e1c6
Paul Nielsen <pacanielsen@gmail.com>
2018-09-25 12:11:36 -0600

    Changed everything back

--- web/week02/team02.js
+++ web/week02/team02.js
@@ -1,17 +1,7 @@
-// ta02.js
-function clickMe() {
+function click(){
 	alert("Clicked!");
 }
 
 function changeColor() {
-	var textbox_id = "txtColor";
-	var textbox = document.getElementById(textbox_id);
-
-	var div_id = "div1";
-	var div = document.getElementById(div_id);
-
-	// We should verify values here before we use them...
-	var color = textbox.value;
-	div.style.backgroundColor = color;
-
+	document.getElementById("first").style.color = color;
 }
