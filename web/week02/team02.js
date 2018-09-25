8b0bd9f97af5a33fa92d0ab1103595720508bdd1
Paul Nielsen <pacanielsen@gmail.com>
2018-09-25 12:25:44 -0600

    change js to look more like the teacher's

--- web/week02/team02.js
+++ web/week02/team02.js
@@ -3,5 +3,12 @@ function clickMe(){
 }
 
 function changeColor() {
+	var textbox_id = "textbox";
+	var textbox = document.getElementById(textbox_id);
 	
+	var div_id = "first";
+	var div = document.getElementById(div_id);
+	
+	var color = textbox.value;
+	div.style.backgroundColor = color;
 }
