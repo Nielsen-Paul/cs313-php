1b8bf0206f24d50459f3bd62c22bdac7f2260e83
Paul Nielsen <pacanielsen@gmail.com>
2018-09-25 12:58:41 -0600

    changed index.html to my homepage

--- web/index.php
+++ web/index.php
@@ -1,5 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>TEAM 02</title>
+<link rel="stylesheet" type="text/css" href="prove02.css">
+</head>
+<body>
+<header></header>
 
-  phpinfo();
 
-?>
+
+
+</body>
+</html>
