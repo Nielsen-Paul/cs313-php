c3d6adef2b06e3d04d550cf48521123461e13477
Paul Nielsen <pacanielsen@gmail.com>
2018-09-25 17:25:56 -0600

    align ninerHeader: try 2

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@
 <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
-<header><img id="ninerHeader" src="49erheader.jpg" alt="49er Header"></header>
-<header>
+<header id="ninerHeader"><img  src="49erheader.jpg" alt="49er Header"></header>
+<header id="navHeader">
 <ul>
   <li><a href="index.php">Home</a></li>
   <li><a href="assignments.php">Assignments</a></li>
