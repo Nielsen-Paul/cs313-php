c57183db5f2d62f35d34e7f4a69b772cec06f6cb
Paul Nielsen <pacanielsen@gmail.com>
2018-09-25 13:13:46 -0600

    add navigation bar: second try

--- web/index.php
+++ web/index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>TEAM 02</title>
-<link rel="stylesheet" type="text/css" href="prove02.css">
+<title>Homepage</title>
+<link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
-<header></header>
+<header>
+<ul>
+  <li><a href="index.php">Home</a></li>
+  <li><a href="assignments.php">Assignments</a></li>
+</ul>
+</header>
 
 
 
