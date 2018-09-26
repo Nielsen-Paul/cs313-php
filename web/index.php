fdc27a92f1bc190bafbe6b51812358c6439e0947
Paul Nielsen <pacanielsen@gmail.com>
2018-09-25 20:10:30 -0600

    bigger jimmyG picture

--- web/index.php
+++ web/index.php
@@ -4,22 +4,26 @@
 <title>Homepage</title>
 <link rel="stylesheet" type="text/css" href="index.css">
 </head>
-<body>
-<header id="ninerHeader"><img  src="49erheader.jpg" alt="49er Header"></header>
-<header id="navHeader">
-<ul>
-  <li><a href="index.php">Home</a></li>
-  <li><a href="assignments.php">Assignments</a></li>
-</ul>
-</header>
-<br>
-<div id="main"><img src="gimmyG.jpg" alt="Garoppolo Hurt"></div>
-<div id="newsFeed">
-<h3>Latest '9er News</h3>
-<a>Gimmy G will miss the rest of the season with torn ACL</a>
-<a>Marquis Goodwin is back</a>
-</div>
+	<body>
+		<header id="ninerHeader">
+			<img  src="49erheader.jpg" alt="49er Header">
+		</header>
+		<header id="navHeader">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="assignments.php">Assignments</a></li>
+		</ul>
+		</header>
+		<br>
+		<div id="main">
+			<img id="jimmyG" src="gimmyG.jpg" alt="Garoppolo Hurt">
+		</div>
+		<div id="newsFeed">
+			<h3>Latest '9er News</h3>
+				<a>Jimmy G will miss the rest of the season with torn ACL</a>
+				<a>Marquis Goodwin is back</a>
+		</div>
 
 
-</body>
+	</body>
 </html>
