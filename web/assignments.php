d9e3b968b5052a3b4fc30e5b49293f97c6f73dc7
Paul Nielsen <pacanielsen@gmail.com>
2018-09-28 19:48:30 -0600

    changed navBar to php

---
+++ web/assignments.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CS313 Assignments</title>
+        <link rel="stylesheet" type="text/css" href="assignments.css">
+    </head>
+	<body>
+        <?php include 'navBar.php';?>
+        <div>
+            <h3>Fall 2018 CS313 Assignments</h3>
+            <p>Week03: Coming Soon</p>
+            <p>Week04: Coming Soon</p>
+            <p>Week05: Coming Soon</p>
+            <p>Week06: Coming Soon</p>
+            <p>Week07: Coming Soon</p>
+            <p>Week08: Coming Soon</p>
+            <p>Week09: Coming Soon</p>
+            <p>Week10: Coming Soon</p>
+            <p>Week11: Coming Soon</p>
+            <p>Week12: Coming Soon</p>
+            <p>Week13: Coming Soon</p>
+            <p>Week14: Coming Soon</p>
+        </div>
+        <?php include 'navBar.php';?>
+    </body>
+</html>
--- web/navBar.php
+++ web/navBar.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Homepage</title>
+        <title>Navigation Bar</title>
         <link rel="stylesheet" type="text/css" href="navBar.css">
     </head>
 	<body>
