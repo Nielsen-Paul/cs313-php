90de173efe8c6cfea1caebb237db90367db3b82c
Paul Nielsen <pacanielsen@gmail.com>
2018-09-28 20:02:49 -0600

    change links on navBar

--- web/navBar.php
+++ web/navBar.php
@@ -6,7 +6,7 @@
     </head>
 	<body>
         <div id="navBar">
-			<p><a href="index.php">Homepage</a><span>"        "</span><a href="assignments.php">Assignments Page</a></p>
+			<p><a href="index.php">Homepage</a><span> </span><a href="assignments.php">Assignments Page</a></p>
         </div>
     </body>
 </html>
