5f610ff63c493fdf89d2af0e54fbde53681888e9
Paul Nielsen <pacanielsen@gmail.com>
2018-09-28 21:09:13 -0600

    space between links

--- web/navBar.php
+++ web/navBar.php
@@ -6,7 +6,7 @@
     </head>
 	<body>
         <div id="navBar">
-			<p><a href="index.php" id="homePage">Homepage</a><a href="assignments.php">Assignments Page</a></p>
+			<p><span id="homePage">Homepage><a href="index.php" </a></span><a href="assignments.php">Assignments Page</a></p>
         </div>
     </body>
 </html>
