626d65cb2ec033483490e070875ba219ef439a9c
Paul Nielsen <pacanielsen@gmail.com>
2018-10-02 18:40:38 -0600

    php file added

---
+++ web/Week03/Team03/myFile.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>myFile03</title>
+    </head>
+    <body>
+        <span>Name<?php echo $_POST["name"]; ?></span><br>
+        <span>Email<?php echo $_POST["email"]; ?></span><br>
+        <?php ?>Computer Science<br>
+        <?php ?>Web Design and Development<br>
+        <?php ?>Computer Information Technology<br>
+        <?php ?>Computer Engineering<br>
+        <?php echo $_POST["comment"]; ?><br>
+    </body>
+</html>    
--- web/Week03/Team03/team03.php
+++ web/Week03/Team03/team03.php
@@ -4,15 +4,16 @@
         <title>Team03</title>
     </head>
     <body>
-        <form>
+        <form action="myFile.php" method="POST">
             <span>Name<input type="text" name="name"></span><br>
             <span>Email<input type="text" name="email"></span><br>
             <input type="radio" name="majors" value="Computer Science">Computer Science<br>
             <input type="radio" name="majors" value="Web Design and Development">Web Design and Development<br>
             <input type="radio" name="majors" value="Computer Information Technology">Computer Information Technology<br>
             <input type="radio" name="majors" value="Computer Engineering">Computer Engineering<br>
-            <textarea name="comment" rows=5 cols=40 placeholder="Comments"></textarea>
-            <input type="submit" name="submit" value="submit">
+            <textarea name="comment" rows=5 cols=40 placeholder="Comments"></textarea><br>
+            <input type="submit" name="submit" value="Submit">
         </form>
+
     </body>
 </html>
