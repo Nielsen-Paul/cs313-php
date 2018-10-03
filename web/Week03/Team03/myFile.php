40e41cbcf2733a496e694f90e8d3dbcaac4c2ae4
Paul Nielsen <pacanielsen@gmail.com>
2018-10-02 18:44:21 -0600

    php file added

--- web/Week03/Team03/myFile.php
+++ web/Week03/Team03/myFile.php
@@ -6,7 +6,7 @@
     <body>
         <span>Name <?php echo $_POST["name"]; ?></span><br>
         <span>Email <?php echo $_POST["email"]; ?></span><br>
-        Major <?php echo $_POST["major"]; ?><br>
+        Major <?php echo $_POST["majors"]; ?><br>
         <?php echo $_POST["comment"]; ?><br>
     </body>
 </html>    
