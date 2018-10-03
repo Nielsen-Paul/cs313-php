caabaa18ef5efe045568246cab4462dcbe6f84b8
Paul Nielsen <pacanielsen@gmail.com>
2018-10-02 18:24:30 -0600

    new file

---
+++ web/Week03/Team03/team03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team03</title>
+    </head>
+    <body>
+        <form>
+            <span>Name<input type="text" name="name"></span>
+            <span>Email<input type="text" name="email"></span>
+            <input type="radio" name="majors" value="Computer Science">Computer Science<br>
+            <input type="radio" name="majors" value="Web Design and Development">Web Design and Development<br>
+            <input type="radio" name="majors" value="Computer Information Technology">Computer Information Technology<br>
+            <input type="radio" name="majors" value="Computer Engineering">Computer Engineering<br>
+            <textarea name="comment" rows=5 cols=40 placeholder="Comments"></textarea>
+            <input type="submit" name="submit" value="submit">
+        </form>
+    </body>
+</html>
