f424d0fe4333758490a3de64d48f718b2dbd16bb
Paul Nielsen <pacanielsen@gmail.com>
2018-10-05 16:51:51 -0600

    creating session for confirmation page

--- web/Week03/Prove03/confirm.php
+++ web/Week03/Prove03/confirm.php
@@ -9,6 +9,12 @@
         <title>Confirmation Page</title>
     </head>
     <body>
-        <h2>Your item has been purchased and will be sent to this address: </h2>
+        <h2>The following items have been purchased: </h2>
+        <?php
+        foreach($_SESSION["items"] as $item) {
+            echo $item . "<br>" ; }
+        ?><br>
+        <h2>They will be sent to the following address: </h2>
+        
     </body>
 </html>
