640c4294f7e3974b75abe37d455eafa56c370a18
Paul Nielsen <pacanielsen@gmail.com>
2018-10-05 18:54:07 -0600

    add address to checkout and confirm page

--- web/Week03/Prove03/confirm.php
+++ web/Week03/Prove03/confirm.php
@@ -20,7 +20,7 @@
         ?><br>
         <h2>They will be sent to the following address: </h2>
             <p>
-                <?php echo $address . " " . <br>
+                <?php echo $address . <br> .
                  $city . ", " //. $state . " " . $zip ?>
             </p>
     </body>
