393f2bb6a5b8b1e27f3eccb7d5f08b042e74226c
Paul Nielsen <pacanielsen@gmail.com>
2018-10-19 17:24:14 -0600

    add login file

---
+++ web/project01/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+
+    <!-- Page title -->
+    <title>Duty to God Login</title>
+</head>
+<body>
+    <?php
+	<div id="login">
+	<h1>Please Log In</h1>
+    <span>Username<input type="text" value="username">
+    <span>Password<input type="text" value="password">
+    </div>
+    ?>
+</body>
+</html>
