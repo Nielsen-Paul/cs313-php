4067d52dfe87230ab25e431e5c9631aa158a7230
Paul Nielsen <pacanielsen@gmail.com>
2018-10-19 19:37:34 -0600

    add checkboxes

--- web/project01/requirements.php
+++ web/project01/requirements.php
@@ -38,11 +38,12 @@
 		$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements');
 		while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 		{
+			$comment = $row['comment'];
 			echo '<strong>' . $row['name'] . ' - </strong>' //. 
 			//echo '<input type="checkbox" name="learn" value="$row['learn']">' . ' ' .
 			//echo '<input type="checkbox" name="act" value="$row['act']">' . ''  . 
 			//echo '<input type="checkbox" name="share" value="$row['share']">' . '' . 
-			echo '<input type="text" name="comment" value="'.$row['comment'].'">' . '<br/>';
+			echo '<input type="text" name="comment" value="'.$comment.'">' . '<br/>';
 		  //echo '<input type="text" name="journal" value="$row['journal']">' . '<br/>'; 
 		}
 
