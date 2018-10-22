41e264affb174ded4af283078a60a4ff77ac944b
Paul Nielsen <pacanielsen@gmail.com>
2018-10-22 15:45:15 -0600

    create choose youth page

---
+++ web/project01/chooseYouth.php
@@ -0,0 +1,52 @@
+<?php 
+	$username = $_POST["username"];
+?>
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+
+    <!-- Page title -->
+    <title>Requirements</title>
+		<link rel="stylesheet" type="text/css" href="project01.css">
+</head>
+<body>
+	<?php include 'navBar.php';?>
+	<h1>Choose a Youth</h1>
+	<?php
+		try
+		{
+		  $dbUrl = getenv('DATABASE_URL');
+
+		  $dbOpts = parse_url($dbUrl);
+
+		  $dbHost = $dbOpts["host"];
+		  $dbPort = $dbOpts["port"];
+		  $dbUser = $dbOpts["user"];
+		  $dbPassword = $dbOpts["pass"];
+		  $dbName = ltrim($dbOpts["path"],'/');
+
+		  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
+		catch (PDOException $ex)
+		{
+		  echo 'Error!: ' . $ex->getMessage();
+		  die();
+		}
+
+		$statement = $db->query('SELECT name FROM youth');?>
+		<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
+			<?php echo '<strong>' . $row['name'] . '</strong>'; ?>
+			<br>
+		<?php endwhile; ?>
+	<br>
+	<div class="footer">
+		<?php include 'navBar.php';?>
+	</div>
+</body>
+</html>
