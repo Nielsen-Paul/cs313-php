6acb5f0ee998b372b17edd53d1835feddd911a6a
Paul Nielsen <pacanielsen@gmail.com>
2018-10-22 16:28:40 -0600

    create choose youth page

--- web/project01/requirements.php
+++ web/project01/requirements.php
@@ -38,18 +38,20 @@
 		  echo 'Error!: ' . $ex->getMessage();
 		  die();
 		}
-
-		$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements');?>
-		<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
-			<?php echo '<strong>' . $row['name'] . ' - </strong>'; ?>
-			<label> Learn - </label><input type="checkbox" name="learn" value="<?php echo($row['learn']); ?>" />
-			<label> Act - </label><input type="checkbox" name="act" value="<?php echo($row['act']); ?>" />
-			<label> Share - </label><input type="checkbox" name="share" value="<?php echo($row['share']); ?>" />
-			<label> Comment - </label><input id="comment" type="text" name="comment" value=" " /><?php echo($row['comment']); ?><br>
-			<label> Journal - </label><input id="journal" type="text" name="journal" value=" " /><br>
-			<?php echo($row['journal']); ?><br>
-			<br>
-		<?php endwhile; ?>
+		<form action="received" method="get">
+			$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements');?>
+			<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
+				<?php echo '<strong>' . $row['name'] . ' - </strong>'; ?>
+				<label> Learn - </label><input type="checkbox" name="learn" value="<?php echo($row['learn']); ?>" />
+				<label> Act - </label><input type="checkbox" name="act" value="<?php echo($row['act']); ?>" />
+				<label> Share - </label><input type="checkbox" name="share" value="<?php echo($row['share']); ?>" />
+				<label> Comment - </label><input id="comment" type="text" name="comment" value=" " /><?php echo($row['comment']); ?><br>
+				<label> Journal - </label><input id="journal" type="text" name="journal" value=" " /><br>
+				<?php echo($row['journal']); ?><br>
+				<br>
+			<?php endwhile; ?>
+			<input type="submit" />
+		</form>
 	<br>
 	<div class="footer">
 		<?php include 'navBar.php';?>
