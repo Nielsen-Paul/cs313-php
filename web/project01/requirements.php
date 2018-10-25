68a641b8a7d1ae3fb4ee41a1fc7522b648a8bb84
Paul Nielsen <pacanielsen@gmail.com>
2018-10-24 22:01:38 -0600

    inputs into database

--- web/project01/requirements.php
+++ web/project01/requirements.php
@@ -1,4 +1,4 @@
-<?php 
+<?php include 'db.php';
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		// Insert into db
@@ -32,27 +32,6 @@
 	<?php include 'navBar.php';?>
 	<h1>Duty to God Requirements</h1>
 	<?php
-		try
-		{
-		  $dbUrl = getenv('DATABASE_URL');
-
-		  $dbOpts = parse_url($dbUrl);
-
-		  $dbHost = $dbOpts["host"];
-		  $dbPort = $dbOpts["port"];
-		  $dbUser = $dbOpts["user"];
-		  $dbPassword = $dbOpts["pass"];
-		  $dbName = ltrim($dbOpts["path"],'/');
-
-		  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-		  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-		catch (PDOException $ex)
-		{
-		  echo 'Error!: ' . $ex->getMessage();
-		  die();
-		}
 		$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements');?>
 		<form action="requirements.php" method="POST">
 			<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
