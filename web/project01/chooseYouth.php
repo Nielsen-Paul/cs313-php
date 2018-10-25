a499fc4d468cf40d1bfc6eab9e80c26695ada288
Paul Nielsen <pacanielsen@gmail.com>
2018-10-25 16:52:51 -0600

    youth id to next page

--- web/project01/chooseYouth.php
+++ web/project01/chooseYouth.php
@@ -41,7 +41,7 @@
 
 		$statement = $db->query('SELECT name FROM youth');?>
 		<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
-			<?php "<a href='requirements.php?id=$row['id']'>$row['name'];" ?>
+			<?php echo "<a href='requirements.php?id=$row['id']'>$row['name'];" ?>
 			<br>
 		<?php endwhile; ?>
 	<br>
