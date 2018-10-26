ac149efaa010419d16f0391f211316c7c45126f1
Paul Nielsen <pacanielsen@gmail.com>
2018-10-26 16:14:33 -0600

    printing from db

--- web/project01/requirements.php
+++ web/project01/requirements.php
@@ -49,8 +49,8 @@
 			$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements WHERE youth_id=:youth_id');
 			$statement->bindValue(':youth_id', $youth_id, PDO::PARAM_INT);
 			$statement->execute();
-			while ($row = $statement->fetchAll(PDO::FETCH_ASSOC)): ?>
-				<?php echo '<strong>' . $row['name'] . ' - </strong>'; ?>
+			while ($row = $statement->fetchAll(PDO::FETCH_ASSOC)):
+				echo '<strong>' . $row['name'] . ' - </strong>'; ?>
 				<label> Learn - </label><input type="checkbox" name="learn" value="<?php echo($row['learn']); ?>" />
 				<label> Act - </label><input type="checkbox" name="act" value="<?php echo($row['act']); ?>" />
 				<label> Share - </label><input type="checkbox" name="share" value="<?php echo($row['share']); ?>" />
