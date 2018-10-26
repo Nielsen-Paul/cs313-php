c86a44f7381e6e342a6fd5c88cd5e67ae9de1bfe
Paul Nielsen <pacanielsen@gmail.com>
2018-10-25 20:56:13 -0600

    requirements page improvements

--- web/project01/navBar.php
+++ web/project01/navBar.php
@@ -8,6 +8,7 @@
         <div id="navBar">
             <p><a href="../../index.php">Homepage</a><span> - </span>
             <a href="../../assignments.php">Assignments Page</a><span> - </span>
+            <a href="chooseYouth.php">Choose Youth</a><span> - </span>
             <a href="login.php">Logout</a>
         </div>
     </body>
