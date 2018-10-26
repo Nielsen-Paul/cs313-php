3e845270b96667e79136ae7952c59d7a69f14805
Paul Nielsen <pacanielsen@gmail.com>
2018-10-26 15:51:39 -0600

    printing from db

--- web/project01/requirements.php
+++ web/project01/requirements.php
@@ -47,18 +47,15 @@
 		<form action="requirements.php" method="POST">
 			<?php
 			$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements');
-			$rows = $statement->fetch(PDO::FETCH_ASSOC);
-			foreach ($rows as $row)
-			{ ?>
+			while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
 				<?php echo '<strong>' . $row['name'] . ' - </strong>'; ?>
 				<label> Learn - </label><input type="checkbox" name="learn" value="<?php echo($row['learn']); ?>" />
 				<label> Act - </label><input type="checkbox" name="act" value="<?php echo($row['act']); ?>" />
 				<label> Share - </label><input type="checkbox" name="share" value="<?php echo($row['share']); ?>" />
 				<label> Comment - </label><input id="comment" type="text" name="comment" value=" " /><?php echo($row['comment']); ?><br>
 				<label> Journal - </label><input id="journal" type="text" name="journal" value=" " /><br>
-				<?php echo($row['journal']); 
-			}
-			?><br>
+				<?php echo($row['journal']); ?><br>
+			<?php endwhile; ?>
 			<input type="submit">
 			<br>
 		</form>
