63df28a5bc1285f557b7c9a6dd97f7a85968f46d
Paul Nielsen <pacanielsen@gmail.com>
2018-10-25 20:44:12 -0600

    requirements page improvements

--- web/project01/requirements.php
+++ web/project01/requirements.php
@@ -38,13 +38,13 @@
 	<?php include 'navBar.php';?>
 	<h1>Duty to God Requirements for 
 		<?php echo $youth_name ?> </h1>
-	<?php
-		$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements WHERE youth_id=:youth_id');
-		$statement->bindValue(':youth_id', $youth_id, PDO::PARAM_INT);
-		$statement->execute();	
-	?>
+
 		<form action="requirements.php" method="POST">
-			<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
+			<?php 
+				$statement = $db->query('SELECT name, learn, act, share, comments, journal FROM requirements WHERE youth_id=:youth_id');
+				$statement->bindValue(':youth_id', $youth_id, PDO::PARAM_INT);
+				$statement->execute();
+				while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
 				<?php echo '<strong>' . $row['name'] . ' - </strong>'; ?>
 				<label> Learn - </label><input type="checkbox" name="learn" value="<?php echo($row['learn']); ?>" />
 				<label> Act - </label><input type="checkbox" name="act" value="<?php echo($row['act']); ?>" />
