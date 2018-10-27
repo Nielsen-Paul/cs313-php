87dee722a9ced4acd829cbd3048050b371c10546
Paul Nielsen <pacanielsen@gmail.com>
2018-10-26 22:24:58 -0600

    add input.php

--- web/project01/input.php
+++ web/project01/input.php
@@ -10,12 +10,12 @@ $journal = htmlspecialchars($_POST['journal']);
 
 $stmt = $db->prepare('UPDATE requirements SET name=:name, learn=:learn, act=:act, share=:share, comments=:comments, journal=:journal WHERE youth_id=:youth_id AND name=:name;');
 $stmt->bindValue(':youth_id', $youth_id, PDO::PARAM_INT);
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':name', $name, PDO::PARAM_INT);
 $stmt->bindValue(':learn', $learn, PDO::PARAM_INT);
 $stmt->bindValue(':act', $act, PDO::PARAM_INT);
 $stmt->bindValue(':share', $share, PDO::PARAM_INT);
-$stmt->bindValue(':comments', $comments, PDO::PARAM_STR);
-$stmt->bindValue(':journal', $journal, PDO::PARAM_STR);
+$stmt->bindValue(':comments', $comments, PDO::PARAM_INT);
+$stmt->bindValue(':journal', $journal, PDO::PARAM_INT);
 $stmt->execute();
 
 $new_page = "requirements.php?id=$youth_id";
