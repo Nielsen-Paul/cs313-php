943998a5ae4684a2971e6444cf3f85a743762b59
Paul Nielsen <pacanielsen@gmail.com>
2018-10-26 21:56:43 -0600

    add input.php

---
+++ web/project01/input.php
@@ -0,0 +1,25 @@
+<?php include 'db.php';
+
+$youth_id = htmlspecialchars($_POST['youth_id']);
+$name = htmlspecialchars($_POST['name']);
+$learn = htmlspecialchars($_POST['learn']);
+$act = htmlspecialchars($_POST['act']);
+$share = htmlspecialchars($_POST['share']);
+$comments = htmlspecialchars($_POST['comments']);
+$journal = htmlspecialchars($_POST['journal']);
+
+$db = get_db();
+
+$stmt = $db->prepare('UPDATE requirements(name, learn, act, share, comments, journal) VALUES (:name, :learn, :act, :share, :comments, :journal) WHERE youth_id=:youth_id AND name=:name;');
+$stmt->bindValue(':youth_id', $youth_id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->bindValue(':learn', $learn, PDO::PARAM_INT);
+$stmt->bindValue(':act', $act, PDO::PARAM_INT);
+$stmt->bindValue(':share', $share, PDO::PARAM_INT);
+$stmt->bindValue(':comments', $comments, PDO::PARAM_STR);
+$stmt->bindValue(':journal', $journal, PDO::PARAM_STR);
+
+$new_page = "requirements.php?id=$youth_id";
+die();
+
+?>
