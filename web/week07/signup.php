fa397384215878cbb6bde94ff006167ae5ec2a75
Paul Nielsen <pacanielsen@gmail.com>
2018-10-30 13:30:50 -0600

    team assignment 07 files

---
+++ web/week07/signup.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+
+        <!-- Page title -->
+        <title>Database Signup</title>
+        <link rel="stylesheet" type="text/css" href="login.css">
+    </head>
+    <body>
+        <div id="login">
+	        <h1>Create a Log In</h1>
+            <form action="requirements.php" method="get">
+                <span>Username<input type="text" name="username" value=""></span><br>
+                <span>Password<input type="text" name="password" value=""></span><br>
+                <input type="submit">
+            </form>
+        </div>
+    </body>
+</html>
