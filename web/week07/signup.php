0d673e84d846bf24006a8a49da99ec93b0d89b20
Paul Nielsen <pacanielsen@gmail.com>
2018-10-30 18:15:46 -0600

    add public.

--- web/week07/signup.php
+++ web/week07/signup.php
@@ -1,3 +1,18 @@
+<?php include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $hashpassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $query = 'INSERT INTO public."user" (name, password) VALUES (:name, :password)';
+    $stmt = $db->prepare($query);
+    $pdo = $stmt->execute(array(':name' => $_POST['name'], ':password' => $_POST['password']));
+
+    $newURL = "./signin.php";
+    header('Location: ' . $newURL);
+}
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
@@ -12,7 +27,7 @@
     <body>
         <div id="login">
 	        <h1>Create an Account</h1>
-            <form action="signin.php" method="POST">
+            <form action="signup.php" method="POST">
                 <span>Username<input type="text" name="name" value=""></span><br>
                 <span>Password<input type="password" name="password" value=""></span><br>
                 <input type="submit">
