95d2d85705c673dbbeb26c910c0ecda32b2df603
Paul Nielsen <pacanielsen@gmail.com>
2018-10-30 18:08:41 -0600

    add public.

--- web/week07/signin.php
+++ web/week07/signin.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $query = 'INSERT INTO user (name, password) VALUES (:name, :password)';
+    $query = 'INSERT INTO public."user" (name, password) VALUES (:name, :password)';
     $stmt = $db->prepare($query);
     $pdo = $stmt->execute(array(':name' => $_POST['name'], ':password' => $_POST['password']));
 
