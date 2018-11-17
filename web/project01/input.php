0e9bff96684dfa58e9a121fc0d7af4d2e37e6cd9
Paul Nielsen <pacanielsen@gmail.com>
2018-11-17 10:03:37 -0700

    duty to God

--- web/project01/input.php
+++ web/project01/input.php
@@ -6,16 +6,14 @@ $learn = $_POST['learn'];
 $act = $_POST['act'];
 $share = $_POST['share'];
 $comments = $_POST['comments'];
-$journal = $_POST['journal'];
 
-$stmt = $db->prepare('UPDATE requirements SET name=:name, learn=:learn, act=:act, share=:share, comments=:comments, journal=:journal WHERE youth_id=:youth_id AND name=:name;');
+$stmt = $db->prepare('UPDATE requirements SET name=:name, learn=:learn, act=:act, share=:share, comments=:comments WHERE youth_id=:youth_id AND name=:name;');
 $stmt->bindValue(':youth_id', $youth_id, PDO::PARAM_INT);
 $stmt->bindValue(':name', $name, PDO::PARAM_INT);
 $stmt->bindValue(':learn', $learn, PDO::PARAM_STR);
 $stmt->bindValue(':act', $act, PDO::PARAM_STR);
 $stmt->bindValue(':share', $share, PDO::PARAM_STR);
 $stmt->bindValue(':comments', $comments, PDO::PARAM_INT);
-$stmt->bindValue(':journal', $journal, PDO::PARAM_INT);
 $stmt->execute();
 
 $new_page = "requirements.php?id=$youth_id";
