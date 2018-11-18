77dd46b1ee1ec2fa60b063edf056ef2d14a67311
Paul Nielsen <pacanielsen@gmail.com>
2018-11-17 20:00:36 -0700

    express app

--- web/myapp/newapp/server.js
+++ web/myapp/newapp/server.js
@@ -1,32 +1,29 @@
-// server.js
-// load the things we need
-var express = require('express');
-var app = express();
+const express = require('express');
+var parser = require('body-parser');
+const path = require('path');
+const rateCalc = require('./calculateRate');
+const PORT = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-// index page 
-app.get('/', function(req, res) {
-  var drinks = [
-      { name: 'Bloody Mary', drunkness: 3 },
-      { name: 'Martini', drunkness: 5 },
-      { name: 'Scotch', drunkness: 10 }
-  ];
-  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-  res.render('pages/index', {
-      drinks: drinks,
-      tagline: tagline
-  });
-});
-
-// about page 
-app.get('/about', function (req, res) {
-  res.render('pages/about');
-});
-
-app.listen(8080);
-console.log('8080 is the magic port');
+express()
+    .use(parser.urlencoded({ extended: false }))
+    .use(parser.json())
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/calculateRate'))
+    .get('/calculateRate', (req, res) => res.render('pages/calculateRate', {
+        type: null,
+        weight: null,
+        rate: null
+    }))
+    .post('/calculateRate', (req, res) => {
+        let type = req.body.type;
+        let weight = req.body.weight;
+        let rate = rateCalc(type, weight);
+        res.render('pages/calculateRate', {
+            type: type,
+            weight : weight,
+            rate: rate
+            });
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
